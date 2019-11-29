37161278209d28ad67980954eb4a886403b3b01b
Maëlys McArdle <airseal@hotmail.com>
2019-11-28 21:58:32 -0500

    Added functionality to analyzer.

--- analyze-pages.py
+++ analyze-pages.py
@@ -7,9 +7,9 @@ def main_function():
     """
     argument_parser, arguments = parse_arguments()
 
-    if arguments.search:
-        execute_command(arguments)
-    else:
+    ran_command = execute_command(arguments)
+
+    if not ran_command:
         argument_parser.print_help()
 
 def parse_arguments():
@@ -21,47 +21,81 @@ def parse_arguments():
         help='Input file for the parsed pages')
     parser.add_argument('--search', default=None,
         help='Text to search in the articles')
+    parser.add_argument('--list', default=None,
+        help='List [title|authors|date|word-count|author|excerpt|content] of the articles')
 
     return parser, parser.parse_args()
 
 def execute_command(arguments):
-
+    """
+    Executes the command.
+    """
     with open(arguments.input_file) as input_file:
         articles_and_metadata = yaml.safe_load(input_file)
 
         if arguments.search:
             search_articles(arguments.search, articles_and_metadata)
+        elif arguments.list:
+            list_property(arguments.list, articles_and_metadata)
+        else:
+            return False
+    
+    return True
 
+def list_property(property, articles_and_metadata):
+    """
+    Lists a given property.
+    """
+    for title in articles_and_metadata:
+        try:
+            print(articles_and_metadata[title][property])
+        except KeyError:
+            valid_properties = ", ".join(articles_and_metadata[title].keys())
+            print(f"'{property}' isn't a valid item to list.")
+            print(f"Choices are: {valid_properties}")
+            return
 
 def search_articles(query, articles_and_metadata):
     """
     Searches the articles for a string.
+    Case-insensitive.
     """
     results = 0
     for title in articles_and_metadata:
         article_content = articles_and_metadata[title]["content"]
-        if query in article_content:
-            show_snippet(query, title, article_content)
+        matches = list(get_matches(query, article_content))
+
+        if matches:
+            print(f"{title}:")
+            for snippet in matches:
+                print(snippet)
+            print()
             results += 1
 
     print(f"Found {results} results.")
 
-def show_snippet(query, title, content):
+def get_matches(query, content):
     """
-    Show a text snippet.
+    Show snippets for the matches.
     """
 
-    margin = 10
+    margin = 30
     query_len = len(query)
     content_len = len(content)
-    
-    match_start = content.find(query)
-    match_end = match_start + query_len
-    snippet_start = match_start - margin if match_start - margin >= 0 else 0
-    snippet_end = match_end + margin if match_end + margin < content_len else content_len - 1
+    match_start = -1
 
-    print(f"{title}:")
-    print(f"   ...{content[snippet_start:snippet_end]}...")
+    while True:
+        match_start = content.upper().find(query.upper(), match_start + 1)
+
+        if match_start == -1:
+            break
+
+        match_end = match_start + query_len
+        snippet_start = match_start - margin if match_start - margin >= 0 else 0
+        snippet_end = match_end + margin if match_end + margin < content_len else content_len - 1
+
+        snippet = content[snippet_start:snippet_end].replace("\n", " ")
+        yield f"   ...{snippet}..."
 
 if __name__ == "__main__":
     main_function()
