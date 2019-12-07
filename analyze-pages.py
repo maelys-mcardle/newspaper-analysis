7c79e32ac4bfcc834e0d26fc4b1bd4328d04cf33
Maëlys McArdle <airseal@hotmail.com>
2019-12-06 20:40:25 -0500

    Added more ways to analyze articles.

--- analyze-pages.py
+++ analyze-pages.py
@@ -1,5 +1,6 @@
 import argparse
 import yaml
+import datetime
 
 def main_function():
     """
@@ -23,9 +24,19 @@ def parse_arguments():
         help='Search for text in the articles')
     parser.add_argument('--list', default=None,
         help='List [title|authors|date|word-count|author|excerpt|content] of the articles')
-    parser.add_argument('--author-count',action='store_true',
+    parser.add_argument('--statistics', action='store_true',
+        help='Gives basic statistics about the articles.')
+    parser.add_argument('--count-articles', action='store_true',
+        help='Counts the total number of articles')
+    parser.add_argument('--count-words', action='store_true',
+        help='Counts the total number of words')
+    parser.add_argument('--count-paragraphs', action='store_true',
+        help='Counts the total number of paragraphs')
+    parser.add_argument('--count-by-author', action='store_true',
         help='Counts the number of articles by each author')
-
+    parser.add_argument('--count-by-year', action='store_true',
+        help='Counts the number of articles bucketed by year')
+ 
     return parser, parser.parse_args()
 
 def execute_command(arguments):
@@ -39,14 +50,95 @@ def execute_command(arguments):
             search_articles(arguments.search, articles_and_metadata)
         elif arguments.list:
             list_property(arguments.list, articles_and_metadata)
-        elif arguments.author_count:
-            author_count(articles_and_metadata)
+        elif arguments.statistics:
+            show_statistics(articles_and_metadata)
+        elif arguments.count_articles:
+            count_articles(articles_and_metadata)
+        elif arguments.count_words:
+            count_words(articles_and_metadata)
+        elif arguments.count_paragraphs:
+            count_paragraphs(articles_and_metadata)
+        elif arguments.count_by_author:
+            count_by_author(articles_and_metadata)
+        elif arguments.count_by_year:
+            count_by_year(articles_and_metadata)
         else:
             return False
     
     return True
 
-def author_count(articles_and_metadata):
+def show_statistics(articles_and_metadata):
+    """
+    Gives basic statistics.
+    """
+    print("BASIC STATISTICS")
+    count_articles(articles_and_metadata)
+    count_paragraphs(articles_and_metadata)
+    count_words(articles_and_metadata)
+    print()
+
+    print("ARTICLES BY YEAR")
+    count_by_year(articles_and_metadata)
+    print()
+
+    print("ARTICLES BY AUTHOR")
+    count_by_author(articles_and_metadata)
+    print()
+
+def count_articles(articles_and_metadata):
+    """
+    Counts the total number of paragraphs written.
+    """
+    print(f"There are {len(articles_and_metadata)} articles.")
+
+def count_paragraphs(articles_and_metadata):
+    """
+    Counts the total number of paragraphs written.
+    """
+    total_paragraphs = 0
+    for title in articles_and_metadata:
+        total_paragraphs += articles_and_metadata[title]['content'].count('\n')
+    print(f"There are {total_paragraphs} paragraphs written.")
+
+def count_words(articles_and_metadata):
+    """
+    Counts the total number of words written.
+    """
+    total_words = 0
+    for title in articles_and_metadata:
+        total_words += articles_and_metadata[title]['word-count']
+    print(f"There are {total_words} words written.")
+
+def count_by_year(articles_and_metadata):
+    """
+    Counts all the articles done each year.
+    """
+    all_dates = get_all_dates(articles_and_metadata)
+    year_count = {}
+
+    for date in all_dates:
+        if date.year in year_count:
+            year_count[date.year] += 1
+        else:
+            year_count[date.year] = 1
+
+    print_all_items_in_dict(year_count)
+
+def get_all_dates(articles_and_metadata):
+    """
+    Gets all the dates from the articles.
+    """
+    all_dates = []
+    for title in articles_and_metadata:
+        date_as_string = articles_and_metadata[title]['date']
+        date_as_datetime = datetime.datetime.strptime(date_as_string, "%Y-%m-%dT%H:%M:%SZ")
+        all_dates.append(date_as_datetime)
+    return all_dates
+
+def count_by_author(articles_and_metadata):
+    """
+    Gets the count of articles done by each author.
+    """
     author_count = {}
 
     for title in articles_and_metadata:
@@ -56,8 +148,19 @@ def author_count(articles_and_metadata):
             else:
                 author_count[author] = author_count[author] + 1
    
-    for author in sorted(author_count):
-        print(f"{author}: {author_count[author]}")
+    print_all_items_in_dict(author_count)
+
+def print_all_items_in_dict(all_items):
+
+    # Find the length of the longest item.
+    longest_item = 0
+    for item in all_items:
+        item_length = len(f"{item}")
+        if item_length > longest_item:
+            longest_item = item_length
+
+    for item in sorted(all_items):
+        print(f"{item}".rjust(longest_item) + f": {all_items[item]}")
 
 def list_property(property, articles_and_metadata):
     """
